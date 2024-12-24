125c2a734a0502932376f5d398dbec32b044f943
t9864 노트북 <qudwn9898@gmail.com>
2024-12-24 17:58:57 +0900

    박병주 2024-12-24 5:58 개인프로젝트 개발 시작

---
+++ src/main/java/com/example/demo/campingcall/CampingCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.campingcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampingCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampingCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/campingcall/CampingCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.campingcall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
