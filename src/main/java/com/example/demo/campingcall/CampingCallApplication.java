668b872b93a8670a07b10fcc4c6e5a260f4a65b4
t9864 노트북 <qudwn9898@gmail.com>
2025-01-21 18:31:00 +0900

    Revert "박병주"
    This reverts commit c7a056803529f34c14781bc79c0e6dc7a6a01f49.

--- src/main/java/com/example/demo/campingcall/CampingCallApplication.java
+++ src/main/java/com/example/demo/campingcall/CampingCallApplication.java
@@ -2,10 +2,9 @@ package com.example.demo.campingcall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CampingCallApplication extends SpringBootServletInitializer{
+public class CampingCallApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CampingCallApplication.class, args);
